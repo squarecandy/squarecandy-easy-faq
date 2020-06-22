6c140913405793947e3c1fd18b69dfbdcd5121f6
Peter Wise <peter@squarecandy.net>
2020-06-22 15:54:20 -0400

    refactor: start adding all ci and linting

---
+++ plugin.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Square Candy Easy FAQ Accordions
+Description: Fork of the Easy FAQ with Expanding Text plugin for use with Theory One Design and Square Candy themes.
+Version: 4.0.1
+Author: bgentry, squarecandy
+Author URI: http://squarecandy.net
+Plugin URI: https://bitbucket.org/squarecandy/squarecandy-easy-faq
+GitHub Plugin URI: https://github.com/squarecandy/squarecandy-easy-faq
+
+Thanks to the original plugin author http://bryangentry.us
+*/
+
+require_once plugin_dir_path( __FILE__ ) . 'functions.php';
